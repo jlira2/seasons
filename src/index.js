1e843490d22f1c832e70e0f0b7659f30b6696caa
Lira, Moises <jesuslira87@gmail.com>
2020-03-15 17:58:29 -0600

    moved season logic over to own component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
 	state = { lat: null, errorMessage: "" };
@@ -15,7 +16,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				Latitude: {this.state.lat}
+				<SeasonDisplay lat={this.state.lat} />
 				<br />
 				Error: {this.state.errorMessage}
 			</div>
