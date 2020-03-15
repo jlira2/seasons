8247aafce17c914bfbcfe657ee721f5254c20dee
Lira, Moises <jesuslira87@gmail.com>
2020-03-15 15:34:00 -0600

    adding, changing and printing state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
-		position => console.log(position),
-		err => console.log(err)
-	);
-	return <div>Hi there!</div>;
-};
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { lat: null };
+		window.navigator.geolocation.getCurrentPosition(
+			position => {
+				this.setState({
+					lat: position.coords.latitude,
+					errorMessage: "",
+				});
+			},
+			err => {
+				this.setState({
+					errorMessage: err.message,
+				});
+			}
+		);
+	}
+	render() {
+		return (
+			<div>
+				Latitude: {this.state.lat}
+				<br />
+				Error: {this.state.errorMessage}
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
